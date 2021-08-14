944a42c69dca6569555223f57534bca1a958f655
Nick <fr13.dev@gmail.com>
2021-08-14 23:40:23 +0300

    refactor google client

--- src/main/java/com/dev/fr13/google/GoogleSheetsClient.java
+++ src/main/java/com/dev/fr13/google/GoogleSheetsClient.java
@@ -5,12 +5,17 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.Sheet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GoogleSheetsClient {
+    private static final Logger log = LoggerFactory.getLogger(GoogleSheetsClient.class);
+
     private static final String APPLICATION_NAME = "Timekeeper";
     private final Credential credential;
     private final String spreadsheetId;
@@ -20,21 +25,36 @@ public class GoogleSheetsClient {
         this.spreadsheetId = spreadsheetId;
     }
 
-    public List<Sheet> getSheets() {
+    public List<String> getSheetsTitles() {
+        var sheets = getSheets();
+        return sheets.stream()
+                .map(i -> i.getProperties().getTitle())
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public List<List<Object>> fetchRangeData(String range) {
+        log.info("Fetch data from {}", range);
         var sheets = createSheets();
         try {
-            var spreadsheet = sheets.spreadsheets().get(spreadsheetId).execute();
-            return spreadsheet.getSheets();
+            var resp = sheets.spreadsheets()
+                    .values()
+                    .get(spreadsheetId, range)
+                    .execute();
+            var res = resp.getValues();
+            if (res != null) {
+                log.info("Found {} rows", res.size());
+            }
+            return res;
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
     }
 
-    public List<List<Object>> fetchRangeData(String range) {
+    private List<Sheet> getSheets() {
         var sheets = createSheets();
         try {
-            var resp = sheets.spreadsheets().values().get(spreadsheetId, range).execute();
-            return resp.getValues();
+            var spreadsheet = sheets.spreadsheets().get(spreadsheetId).execute();
+            return spreadsheet.getSheets();
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }

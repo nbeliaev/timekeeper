9e3e82cf351f20d3c4436972c8f46f9c53776182
Nick <fr13.dev@gmail.com>
2021-08-14 23:39:47 +0300

    refactor google credential

--- src/main/java/com/dev/fr13/google/GoogleCredential.java
+++ src/main/java/com/dev/fr13/google/GoogleCredential.java
@@ -1,5 +1,6 @@
 package com.dev.fr13.google;
 
+import com.dev.fr13.google.exception.FailedGetCredential;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -9,6 +10,10 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.SheetsScopes;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,28 +22,32 @@ import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
+@Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GoogleCredential {
 
-    public static Credential getCredential() throws IOException, GeneralSecurityException {
-        //var in = TimeKeeper.class.getResourceAsStream("/client_secret.json");
+    public Credential getCredential() {
         var in = GoogleCredential.class.getResourceAsStream("/client_secret.json");
         if (in == null) {
             throw new IllegalStateException("Couldn't load Google client secret file");
         }
-        var clientSecrets = GoogleClientSecrets.load(
-                JacksonFactory.getDefaultInstance(),
-                new BufferedReader(new InputStreamReader(in))
-        );
+        try {
+            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                    JacksonFactory.getDefaultInstance(),
+                    new BufferedReader(new InputStreamReader(in)));
 
-        var flow = new GoogleAuthorizationCodeFlow.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                clientSecrets,
-                List.of(SheetsScopes.SPREADSHEETS)
-        ).setDataStoreFactory(new FileDataStoreFactory(new File("tokens")))
-                .setAccessType("offline")
-                .build();
+            var flow = new GoogleAuthorizationCodeFlow.Builder(
+                    GoogleNetHttpTransport.newTrustedTransport(),
+                    JacksonFactory.getDefaultInstance(),
+                    clientSecrets,
+                    List.of(SheetsScopes.SPREADSHEETS)
+            ).setDataStoreFactory(new FileDataStoreFactory(new File("tokens")))
+                    .setAccessType("offline")
+                    .build();
 
-        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+            return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        } catch (IOException | GeneralSecurityException e) {
+            throw new FailedGetCredential(e);
+        }
     }
 }
